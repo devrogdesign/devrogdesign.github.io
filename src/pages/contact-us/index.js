ae631e269a225061e6bd10931bce4a5453c7e76e
lgramirez <gramirez.developer@gmail.com>
2021-01-14 10:11:50 -0400

    Add link to contact-us

---
+++ src/pages/contact-us/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../components/Layout"
+import About from "../../components/About"
+import Hero from "../../components/Hero"
+import SEO from "../../components/seo"
+import Services from "../../components/Services"
+
+const ContactUs = () => {
+  return <Layout></Layout>
+}
+
+export default ContactUs
