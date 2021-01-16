434fb85485e23416560e41a3e88b39f6e1b07994
devrg5 <rg--5@hotmail.com>
2021-01-16 15:56:15 -0400

    Add interaction menu mobile

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,36 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import Content from "./content"
 
-const NavHeader = () => {
-    const state = { clicked: false }
+class NavHeader extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { clicked: false }
+    }
 
-    const handleClick = () => {
-    this.setState({ clicked: !state.clicked })
+    handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
   }
 
-  return (
-    <nav className="nav bd-container">
-      <span href="#" className="nav__logo"></span>
-      
-      <div className="nav__menu" id="nav-menu">
-        <ul className="nav__list">
-          {Content.map((item, index) => (
-            <li key={index} className="nav__item">
-              <Link to={item.url} className="nav__link">
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="nav__toggle" id="nav-toggle">
-      <div className="box__icon">
-        <box-icon name="menu-alt-right" type="regular" color="#010101" size="1.2rem"></box-icon>
-        </div>
-      </div>
-    </nav>
-  )
+  render() {
+    return (
+        <nav className="nav bd-container">
+          <span href="#" className="nav__logo"></span>
+          
+          <div className={this.state.clicked ? "nav__menu active" : "nav__menu"} id="nav-menu">
+            <ul className="nav__list">
+              {Content.map((item, index) => (
+                <li key={index} className="nav__item">
+                  <Link to={item.url} className="nav__link">
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="nav__toggle" id="nav-toggle" onClick={this.handleClick}>
+          <div className="box__icon">
+            <box-icon name={this.state.clicked ? "x" : "menu-alt-right"} type="regular" color="#010101" size="1.2rem"></box-icon>
+            </div>
+          </div>
+        </nav>
+      )
+  }
 }
 
 export default NavHeader
